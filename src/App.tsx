a218d55d59a9805286ed3643fbc3ac950fe04f6c
Mohamed Adel <mohamed101291@gmail.com>
2023-12-12 16:34:21 +0200

    Building Game Card

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             <Show above="lg">
                 <GridItem area="aside">Aside</GridItem>
             </Show>
-            <GridItem area="main">
+            <GridItem area="main" >
                 <GameGrid />
             </GridItem>
         </Grid>
