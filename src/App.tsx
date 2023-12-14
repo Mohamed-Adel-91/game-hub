c962a179ec739ef655220f4f82f1cbfb14b15ad4
Mohamed Adel <mohamed101291@gmail.com>
2023-12-14 17:06:27 +0200

    sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, VStack, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,10 +6,12 @@ import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
+    sortOrder: string;
 }
 
 function App() {
@@ -39,14 +41,25 @@ function App() {
                 </GridItem>
             </Show>
             <GridItem area="main">
-                <VStack alignItems={"start"} paddingX={10} paddingY={2}>
+                <HStack
+                    alignItems={"start"}
+                    paddingX={10}
+                    paddingY={2}
+                    marginBottom={5}
+                >
                     <PlatformSelector
                         selectedPlatform={gameQuery.platform}
                         onSelectPlatform={(platform) =>
                             setGameQuery({ ...gameQuery, platform })
                         }
                     />
-                </VStack>
+                    <SortSelector
+                        sortOrder={gameQuery.sortOrder}
+                        onSelectSortOrder={(sortOrder) =>
+                            setGameQuery({ ...gameQuery, sortOrder })
+                        }
+                    />
+                </HStack>
                 <GameGrid gameQuery={gameQuery} />
             </GridItem>
         </Grid>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
@@ -6,6 +6,14 @@ const NavBar = () => {
     return (
         <HStack justifyContent={"space-between"} padding={"10px"}>
             <Image src={logo} boxSize={"60px"} />
+            <HStack>
+                <Text fontSize={"5xl"} fontWeight={"bold"}>
+                    Game Hub
+                </Text>
+                <Text fontSize={"sm"} paddingTop={"30px"}>
+                    Created By : Mohamed Adel
+                </Text>
+            </HStack>
             <ColorModeSwitch />
         </HStack>
     );
