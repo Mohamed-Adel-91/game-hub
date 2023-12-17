e6aac560b87735e0575a3a2aa3403c3a30104387
Mohamed Adel <mohamed101291@gmail.com>
2023-12-17 15:49:45 +0200

    Searching  Games

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
     sortOrder: string;
+    searchText: string;
 }
 
 function App() {
@@ -29,7 +30,11 @@ function App() {
             }}
         >
             <GridItem area="nav">
-                <NavBar />
+                <NavBar
+                    onSearch={(searchText) =>
+                        setGameQuery({ ...gameQuery, searchText })
+                    }
+                />
             </GridItem>
             <Show above="lg">
                 <GridItem area="aside" paddingX={5}>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,8 @@ useData<Game>('/games', {
     params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
     }},
     [ gameQuery]);
 
